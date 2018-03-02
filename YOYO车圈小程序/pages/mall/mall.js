ca83dac8db1b2380ec73f8c14f221ae105058986
lvshenglin <15308077613@163.com>
2018-03-02 18:26:53 +0800

    2018/3/2修改

--- YOYO车圈小程序/pages/mall/mall.js
+++ YOYO车圈小程序/pages/mall/mall.js
@@ -5,7 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    new_info : [
+      '这是新闻',
+      '这还是新闻'
+    ],
+    go_list : [
+      {
+        img_url: './img/jifen.png',
+        text : '可用积分',
+        href : ''
+      },
+      {
+        img_url: './img/duihuan.png',
+        text: '兑换记录',
+        href: ''
+      },
+      {
+        img_url: './img/zhuanjifen.png',
+        text: '如何赚积分',
+        href: ''
+      }
+
+    ],
   },
 
   /**
@@ -62,5 +83,22 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+
+  /**
+   * 点击触发
+   */
+  checkMe : function (data){
+    console.log('点击了'+JSON.stringify(data,null,2));
+  },
+
+  /**
+   * 请求得到商品数据
+   */
+  getGoodsData : function(){
+    wx.request({
+      url: 'http://test.happyinstallment.com/yfsd/',
+    });
   }
 })
